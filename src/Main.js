7f343aa1b7f2555ce684c06832ddc50d15ee2eb5
Caroline Rodrigues <caroline101910@hotmail.com>
2025-08-03 21:45:11 -0300

    Estilo do main e ajuste final da imagem

--- src/Main.js
+++ src/Main.js
@@ -1,14 +1,24 @@
 import React from "react";
+import imagemParaMain from './assets/restauranfood.jpg'
 
 function Main() {
     return (
         <main>
             <div className="main-content-wrapper">
+
+                <div className="text-content">
             <h1>Little Lemon</h1>
             <h2>Chicago</h2>
             <p>Bringing the sunny and authentic flavors of the Mediterranean to the heart of Chicago. Little Lemon combines traditional recipes with a contemporary touch in a welcoming atmosphere. A genuinely fresh experience awaits you.</p>
             <button>Reserve a table</button>
+                </div>
+            
+            <div className="image-content">
+            <img src={imagemParaMain} alt="A delicious dish from Little Lemon restaurant" className="main-image"></img>
             </div>
+            
+            </div>
+        
         </main>
     );
 }
